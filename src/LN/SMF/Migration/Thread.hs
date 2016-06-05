3ecea452caf77bf370717a8e75151ebfa3801681
Andrew Darqui <andrew.darqui@gmail.com>
2016-06-05 01:25:11 -0400

    need to grab things in chunks, otherwise memory dies

--- src/LN/SMF/Migration/Thread.hs
+++ src/LN/SMF/Migration/Thread.hs
@@ -35,63 +35,68 @@ createLegacyThreads = do
   mysql <- asks rMySQL
   limit <- asks rLimit
 
-  threads <- liftIO $ query mysql "select smf_topics.id_topic, smf_topics.is_sticky, smf_topics.id_board, smf_topics.id_member_started, smf_topics.locked, smf_messages.subject, smf_messages.poster_time, smf_messages.poster_ip from smf_topics INNER JOIN smf_messages ON smf_topics.id_first_msg=smf_messages.id_msg where smf_topics.id_board != 79 LIMIT ?" (Only limit)
+  [Only threads_count] <- liftIO $ query_ mysql "select count(*) from smf_messages"
 
-  thread_ids <- smfIds "threadsName"
 
---  forum_ids <- head <$> lnIds "forums"
+  forM_ [0..(threads_count `div` 50)] (\off -> do
+    threads <- liftIO $ query mysql "select smf_topics.id_topic, smf_topics.is_sticky, smf_topics.id_board, smf_topics.id_member_started, smf_topics.locked, smf_messages.subject, smf_messages.poster_time, smf_messages.poster_ip from smf_topics INNER JOIN smf_messages ON smf_topics.id_first_msg=smf_messages.id_msg where smf_topics.id_board != 79 LIMIT ? OFFSET ?" (50 :: Int, (50 * off) :: Int)
 
-  forM_
-    (filter (\(id_topic, _, _, _, _, _, _, _) -> not $ id_topic `elem` thread_ids) threads)
-    (\(
-        id_topic :: Int64,
-        is_sticky :: Bool,
-        id_board :: Int64,
-        id_member_started :: Int64,
-        locked :: Bool,
-        subject :: Text,
-        poster_time :: Int64,
-        poster_ip :: Text
-      ) -> do
+    thread_ids <- smfIds "threadsName"
 
-        liftIO $ print (id_topic, is_sticky, id_board, id_member_started, locked, subject, poster_time, poster_ip)
+  --  forum_ids <- head <$> lnIds "forums"
 
-        mboard <- findLnIdFromSmfId "boardsName" id_board
-        mtopic <- findLnIdFromSmfId "threadsName" id_topic
-        muser  <- findLnIdFromSmfId "usersName" id_member_started
+    forM_
+      (filter (\(id_topic, _, _, _, _, _, _, _) -> not $ id_topic `elem` thread_ids) threads)
+      (\(
+          id_topic :: Int64,
+          is_sticky :: Bool,
+          id_board :: Int64,
+          id_member_started :: Int64,
+          locked :: Bool,
+          subject :: Text,
+          poster_time :: Int64,
+          poster_ip :: Text
+        ) -> do
 
-        liftIO $ print (mboard, mtopic, muser)
+          liftIO $ print (id_topic, is_sticky, id_board, id_member_started, locked, subject, poster_time, poster_ip)
 
-        case (mboard, mtopic, muser) of
+          mboard <- findLnIdFromSmfId "boardsName" id_board
+          mtopic <- findLnIdFromSmfId "threadsName" id_topic
+          muser  <- findLnIdFromSmfId "usersName" id_member_started
 
-          (Nothing, _, _) -> return ()
+          liftIO $ print (mboard, mtopic, muser)
 
-          (_, _, Nothing) -> return ()
+          case (mboard, mtopic, muser) of
 
-          ((Just board), Nothing, (Just user)) -> do
-            -- doesn't exist, created it
-            --
-            eresult <- liftIO $ rw (postThread_ByBoardId [UnixTimestamp $ fromIntegral poster_time] board $
-              ThreadRequest (sanitizeHtml subject) Nothing is_sticky locked Nothing Nothing []) (BSC.pack $ show user)
+            (Nothing, _, _) -> return ()
 
-            case eresult of
-              (Left err) -> do
-                -- HACKJOB, try and create the thread again, but this time with a modified title
-                liftIO $ print err
-                eresult <- liftIO $ rw (postThread_ByBoardId [UnixTimestamp $ fromIntegral poster_time] board $
-                  ThreadRequest (sanitizeHtml $ subject <> (T.pack $ show poster_time)) Nothing is_sticky locked Nothing Nothing []) (BSC.pack $ show user)
+            (_, _, Nothing) -> return ()
 
-                case eresult of
-                  (Left err) -> liftIO $ print err
-                  (Right thread_response) -> do
-                    createRedisMap "threadsName" id_topic (threadResponseId thread_response)
+            ((Just board), Nothing, (Just user)) -> do
+              -- doesn't exist, created it
+              --
+              eresult <- liftIO $ rw (postThread_ByBoardId [UnixTimestamp $ fromIntegral poster_time] board $
+                ThreadRequest (sanitizeHtml subject) Nothing is_sticky locked Nothing Nothing []) (BSC.pack $ show user)
 
-              (Right thread_response) -> do
-                createRedisMap "threadsName" id_topic (threadResponseId thread_response)
+              case eresult of
+                (Left err) -> do
+                  -- HACKJOB, try and create the thread again, but this time with a modified title
+                  liftIO $ print err
+                  eresult <- liftIO $ rw (postThread_ByBoardId [UnixTimestamp $ fromIntegral poster_time] board $
+                    ThreadRequest (sanitizeHtml $ subject <> (T.pack $ show poster_time)) Nothing is_sticky locked Nothing Nothing []) (BSC.pack $ show user)
 
-          (_, _, _) -> return ()
+                  case eresult of
+                    (Left err) -> liftIO $ print err
+                    (Right thread_response) -> do
+                      createRedisMap "threadsName" id_topic (threadResponseId thread_response)
 
-    )
+                (Right thread_response) -> do
+                  createRedisMap "threadsName" id_topic (threadResponseId thread_response)
+
+            (_, _, _) -> return ()
+
+      )
+   )
 
   return ()
 
