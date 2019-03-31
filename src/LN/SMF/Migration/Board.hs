9c4af0168174ae46ec997088df07abdfc4acbb0a
Andrew Darqui <andrew.darqui@gmail.com>
2019-03-30 23:23:41 -0400

    getting rid of creating categories as boards .. used to have a parent/child relationship there. instead, we need to create sections and post boards into sections.

--- src/LN/SMF/Migration/Board.hs
+++ src/LN/SMF/Migration/Board.hs
@@ -35,96 +35,54 @@ createSmfBoards = do
 
   mysql <- asks rMySQL
 
-  categories <- liftIO $ query_ mysql "select id_cat, name from smf_categories ORDER BY id_cat ASC"
-
   board_ids <- smfIds "boardsName"
 
-  liftIO $ putStrLn $ show categories
   liftIO $ putStrLn $ show board_ids
 
+  boards <- liftIO $ query_ mysql "select id_board, id_parent, id_cat, name, description from smf_boards"
+
   forM_
-    (filter (\(id_cat, _) -> not $ id_cat `elem` board_ids) categories)
-    $ \(id_cat :: Int64, name :: Text) -> do
+    (filter (\(id_board, _, _, _, _) -> not $ id_board `elem` board_ids) boards)
+    $ \(id_board  :: Int64,
+       id_parent  :: Int64,
+       id_cat     :: Int64,
+       board_name :: Text,
+       board_desc :: Text
+      ) -> do
 
-      liftIO $ print $ (id_cat, name)
+      let desc = if board_desc == ""
+                    then Nothing
+                    else Just $ sanitizeHtml $ Text.take 132 board_desc
 
-      lr <- rd' $ ApiS.getBoardPack' $ toSafeUrl name
+      liftIO $ print (id_board, board_name, board_desc)
+
+      lr <- rd' $ ApiS.getBoardPack' $ toSafeUrl board_name
       case lr of
         Left err -> error $ show err
         Right (Right BoardPackResponse{..}) -> do
           let BoardResponse{..} = boardPackResponseBoard
-          createRedisMap "boardsName" id_cat boardResponseId
+          createRedisMap "boardsName" id_board boardResponseId
 
         Right _ -> do
-          e_result <- rd (postBoard_ByForumId [UnixTimestamp $ read "1240177678"] defaultForumId $ BoardRequest {
-            boardRequestDisplayName = name,
-            boardRequestDescription = Nothing,
-            boardRequestBoardType = FixMe,
-            boardRequestActive = True,
-            boardRequestIsAnonymous = False,
-            boardRequestCanCreateBoards = False,
-            boardRequestCanCreateThreads = True,
-            boardRequestVisibility = Public,
-            boardRequestIcon = Nothing,
-            boardRequestTags = [],
-            boardRequestGuard = 0
-          })
+          e_result <- rd (postBoard [UnixTimestamp $ read "1240177678"] $
+            BoardRequest {
+              boardRequestDisplayName = sanitizeHtml board_name,
+              boardRequestDescription = desc,
+              boardRequestBoardType = FixMe,
+              boardRequestActive = True,
+              boardRequestIsAnonymous = False,
+              boardRequestCanCreateBoards = False,
+              boardRequestCanCreateThreads = True,
+              boardRequestVisibility = Public,
+              boardRequestIcon = Nothing,
+              boardRequestTags = [],
+              boardRequestGuard = 0
+            })
+
           case e_result of
-            Left err                -> error $ show err
-            Right BoardResponse{..} -> do
-              createRedisMap "boardsName" id_cat boardResponseId
-
-
-  forM_
-    categories
-    $ \(cat_id :: Int64, _) -> do
-
-      boards <- liftIO $ query mysql "select id_board, id_parent, id_cat, name, description from smf_boards where id_cat = ?" (Only cat_id)
-
-      forM_
-        (filter (\(id_board, _, _, _, _) -> not $ id_board `elem` board_ids) boards)
-        $ \(id_board  :: Int64,
-           id_parent  :: Int64,
-           id_cat     :: Int64,
-           board_name :: Text,
-           board_desc :: Text
-          ) -> do
-
-          let desc = if board_desc == ""
-                        then Nothing
-                        else Just $ sanitizeHtml $ Text.take 132 board_desc
-
-          liftIO $ print (id_board, board_name, board_desc)
-
-          mresult <- findLnIdFromSmfId "boardsName" id_board
-
-          lr <- rd' $ ApiS.getBoardPack' $ toSafeUrl board_name
-          case lr of
             Left err -> error $ show err
-            Right (Right BoardPackResponse{..}) -> do
-              let BoardResponse{..} = boardPackResponseBoard
-              createRedisMap "boardsName" id_board boardResponseId
-
-            Right _ -> do
-              e_result <- rd (postBoard [UnixTimestamp $ read "1240177678"] $
-                BoardRequest {
-                  boardRequestDisplayName = sanitizeHtml board_name,
-                  boardRequestDescription = desc,
-                  boardRequestBoardType = FixMe,
-                  boardRequestActive = True,
-                  boardRequestIsAnonymous = False,
-                  boardRequestCanCreateBoards = False,
-                  boardRequestCanCreateThreads = True,
-                  boardRequestVisibility = Public,
-                  boardRequestIcon = Nothing,
-                  boardRequestTags = [],
-                  boardRequestGuard = 0
-                })
-
-              case e_result of
-                Left err -> error $ show err
-                Right child_board_response -> do
-                  createRedisMap "boardsName" id_board (boardResponseId child_board_response)
+            Right child_board_response -> do
+              createRedisMap "boardsName" id_board (boardResponseId child_board_response)
 
   pure ()
 
